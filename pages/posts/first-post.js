8b1b3031757bd27f864f921561f8b70b0aacfc74
Javier Solera <javier.solera@endalia.com>
2023-08-01 16:10:35 +0200

    Primera subida

---
+++ pages/posts/first-post.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+      <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <img src="/images/profile.jpg" alt="Your Name" />
+      <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
