526bac32b2bfc77bdc04b3f9a5dbfe642feb9bee
Javier Solera <javier.solera@endalia.com>
2023-08-02 14:57:50 +0200

    Estilos

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <>
+      <Layout>
       <Head>
       <title>First Post</title>
       </Head>
@@ -17,16 +19,12 @@ export default function FirstPost() {
         }
       />
       <h1>First Post</h1>
-      <img src="/images/profile.jpg" alt="Your Name" />
-      <Image
-    src="/images/profile.jpg" // Route of the image file
-    height={144} // Desired size with correct aspect ratio
-    width={144} // Desired size with correct aspect ratio
-    alt="Your Name"
-  />
+
+
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      </Layout>
     </>
   );
 }
